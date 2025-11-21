48ff3fb9d3ab18eabf6f080b83fe1484bea47412
Scott Fairclough <scott@hexosoft.co.uk>
2025-11-21 11:07:48 +0000

    removing some debug lines from prover program

--- crates/aggchain-proof-core/src/proof.rs
+++ crates/aggchain-proof-core/src/proof.rs
@@ -33,7 +33,6 @@ pub struct AggchainProofWitness {
 impl AggchainProofWitness {
     pub fn verify_aggchain_inputs(&self) -> Result<AggchainProofPublicValues, ProofError> {
         // Verify the FEP proof or ECDSA signature.
-        println!("Verifying info root stuff");
         self.fep.verify(
             self.l1_info_root,
             self.new_local_exit_root,
