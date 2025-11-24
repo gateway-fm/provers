9229a93bfa186a2249715d9dafdb9211bc2a92d8
Scott Fairclough <scott@hexosoft.co.uk>
2025-11-24 14:49:12 +0000

    load in erigon ELF programs for verifying proposer proofs

--- crates/proposer-elfs/build.rs
+++ crates/proposer-elfs/build.rs
@@ -1,13 +1,16 @@
 fn main() {
+    let erigon_bytes = include_bytes!("./cdk-range");
+    let erigon_agg = include_bytes!("./cdk-aggregation");
+
     color_eyre::install().unwrap();
     prover_elf_utils::ElfInfo::writing_to("vkeys_raw.rs")
         // Verification keys for aggregation proof
-        .module("aggregation", op_succinct_elfs::AGGREGATION_ELF)
+        .module("aggregation", erigon_agg)
         .emit_vkey_bytes()
         .emit_vkey_hash()
         .finish()
         // Verification keys for range proof
-        .module("range", op_succinct_elfs::RANGE_ELF_EMBEDDED)
+        .module("range", erigon_bytes)
         .emit_vkey_bytes()
         .emit_vkey_hash()
         .emit_vkey_commitment()
